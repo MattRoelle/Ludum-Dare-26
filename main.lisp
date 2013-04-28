1622fe769bf52006973342859f7972dd7671b013
matt <matt@beast.(none)>
2013-04-28 10:26:01 -0700

    refined controls, added more ai

--- main.lisp
+++ main.lisp
@@ -5,14 +5,44 @@
 (setq lives 3)
 (setq bullets (list))
 (setq enemies (list))
+(setq ebullets (list))
 
 (setq fire-time 0)
 
 
-;enemy logic definitions
+;enemy movement definitions
 (setq wave #'(lambda (e)
                (setf (first e) (+ (first e) (* 7 (cos (/ (get-internal-real-time) 500)))))
                (setf (second e) (+ (second e) 1))))
+
+(setq sprint #'(lambda (e)
+                 (setf (second e) (+ (second e) 5))))
+
+(setq right-strafe #'(lambda (e)
+                       (cond
+                         ((< (second e) 200)
+                          (setf (second e) (+ (second e) 4))
+                          (setf (first e) (+ (first e) .5)))
+                         (t
+                          (setf (first e) (+ (first e) 5))
+                          (setf (second e) (+ (second e) .5))))))
+
+(setq left-strafe #'(lambda (e)
+                       (cond
+                         ((< (second e) 200)
+                          (setf (second e) (+ (second e) 4))
+                          (setf (first e) (+ (first e) .5)))
+                         (t
+                          (setf (first e) (- (first e) 5))
+                          (setf (second e) (+ (second e) .5))))))
+;enemy shooting definitions
+(setq semi-auto
+      #'(lambda (e) 
+          (when (>= (- (get-internal-real-time) (seventh e)) 1000)
+                      (setq ebullets (append
+                                      ebullets
+                                      (list (list (+ 12 (first e)) (+ (second e) (fourth e)) 8 16))))
+                      (setf (seventh e) (get-internal-real-time)))))
 
 (defmacro restartable (&body body)
     `(restart-case
@@ -48,30 +78,53 @@
 (defun logic ()
   ;logic
   
-  (when (sdl:get-key-state :sdl-key-right)
-     (setf (first player) (+ (first player) 6)))
-  (when (sdl:get-key-state :sdl-key-left)
-     (setf (first player) (- (first player) 6))) 
-  (when (sdl:get-key-state :sdl-key-up)
-    (setf (second player) (- (second player) 6)))
-  (when (Sdl:get-key-state :sdl-key-down)
-    (setf (second player) (+ (second player) 6)))
-  
-  (when (sdl:get-key-state :sdl-key-space)
-     (when (>= (- (get-internal-real-time) fire-time) 200)
-       (setq bullets (append
-                       bullets
-                       (list (list (+ 12 (first player)) (- (second player) 16) 8 16))))
-       (setq fire-time (get-internal-real-time))))
+  (let ((keystate (sdl:get-keys-state)))  
+    (cond
+      
+      ((and (find :sdl-key-right keystate) (find :sdl-key-up keystate))
+       (setf (first player) (+ (first player) 5.5))
+       (setf (second player) (- (second player) 5.5)))
+      ((and (find :sdl-key-left keystate) (find :sdl-key-up keystate))
+       (setf (first player) (- (first player) 5.5))
+       (setf (second player) (- (second player) 5.5)))
+      ((and (find :sdl-key-right keystate) (find :sdl-key-down keystate))
+       (setf (first player) (+ (first player) 5.5))
+       (setf (second player) (+ (second player) 5.5)))
+      ((and (find :sdl-key-left keystate) (find :sdl-key-down keystate))
+       (setf (first player) (- (first player) 5.5))
+       (setf (second player) (+ (second player) 5.5)))     
+      
+      ((find :sdl-key-right keystate)
+       (setf (first player) (+ (first player) 8)))
+      ((find :sdl-key-left keystate)
+       (setf (first player) (- (first player) 8))) 
+      ((find :sdl-key-up keystate)
+       (setf (second player) (- (second player) 8)))
+      ((find :sdl-key-down keystate)
+       (setf (second player) (+ (second player) 8))))
     
-  (mapc #'(lambda (i)
+    (when (sdl:get-key-state :sdl-key-space)
+         (when (>= (- (get-internal-real-time) fire-time) 200)
+         (setq bullets (append
+                         bullets
+                         (list (list (+ 12 (first player)) (- (second player) 16) 8 16))))
+        (setq fire-time (get-internal-real-time)))))
+    
+   (mapc #'(lambda (i)
            (if (or (<= (second i) (- 16)) (>= (second i) 640))
              (setq bullets (remove i bullets))
              (setf (second i) (- (second i) 8))))
-       bullets)
+       bullets) 
   
+  (mapc #'(lambda (i)
+           (if (or (<= (second i) (- 16)) (>= (second i) 640))
+             (setq ebullets (remove i ebullets))
+             (setf (second i) (+ (second i) 8))))
+       ebullets) 
+   
   (mapc #'(lambda (e)
             (funcall (fifth e) e)
+            (funcall (sixth e) e)
             (mapc #'(lambda (b)
                       (cond
                         ((rect-collide? (subseq e 0 4) (subseq b 0 4))
@@ -90,12 +143,15 @@
           bullets)
     (mapc #'(lambda (i)
               (apply 'drawq (subseq i 0 4)))
+          ebullets)
+    (mapc #'(lambda (i)
+              (apply 'drawq (subseq i 0 4)))
           enemies))
   (gl:flush)
   (sdl:update-display)
   (sleep (/ 60 10000)))
 
-(setq enemies (append enemies (list (list 300 100 32 32 wave))))
+(setq enemies (append enemies (list (list 300 100 32 32 right-strafe semi-auto 0))))
 
 (defun main ()
   (sdl:with-init ()
